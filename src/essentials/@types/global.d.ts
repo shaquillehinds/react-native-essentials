f1e4bc3afe28f86c9e3a4c3ae769f4700f4fe08d
Shaquille <shaqdulove@hotmail.com>
2025-04-07 16:49:35 -0400

    updated exports

--- src/essentials/@types/global.d.ts
+++ src/essentials/@types/global.d.ts
@@ -4,10 +4,10 @@ type GlobalIs = <T>(value: T) => NonFalsy<T> | undefined;
 type FilterMapFunc<T, E> = (arr: T, index: number) => E;
 
 declare namespace globalThis {
-  var is: GlobalIs;
-  var isDef: (value: unknown) => boolean;
-  var errMsg: (error: unknown) => string;
-  interface Array<T> {
+  export var is: GlobalIs;
+  export var isDef: (value: unknown) => boolean;
+  export var errMsg: (error: unknown) => string;
+  export interface Array<T> {
     filterMap<E>(func: FilterMapFunc<T, E>): NonNullable<E>[];
   }
 }
--- src/essentials/utils/index.ts
+++ src/essentials/utils/index.ts
@@ -3,3 +3,13 @@ export { measureAsync } from './measureAsync';
 export { getSequantialRandomId } from './randomSequentialId';
 export * as Scheduler from './Scheduler';
 export { wait } from './wait';
+export * from './layout';
+
+export declare namespace globalThis {
+  export var is: GlobalIs;
+  export var isDef: (value: unknown) => boolean;
+  export var errMsg: (error: unknown) => string;
+  export interface Array<T> {
+    filterMap<E>(func: FilterMapFunc<T, E>): NonNullable<E>[];
+  }
+}
