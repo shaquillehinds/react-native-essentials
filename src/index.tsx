aec79e76c717cd5ff7a3263f15712d22cf85f0a0
Shaquille <shaqdulove@hotmail.com>
2025-04-07 16:50:14 -0400

    updated exports

--- src/index.tsx
+++ src/index.tsx
@@ -6,3 +6,12 @@ export * from './essentials/styles';
 export * from './essentials/utils';
 import './essentials/utils/global';
 export const globalEssentials = require('./essentials/utils/global');
+
+export declare namespace globalThis {
+  export var is: GlobalIs;
+  export var isDef: (value: unknown) => boolean;
+  export var errMsg: (error: unknown) => string;
+  export interface Array<T> {
+    filterMap<E>(func: FilterMapFunc<T, E>): NonNullable<E>[];
+  }
+}
