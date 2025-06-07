a4b70e22d97b962beb2546ec769f8d63d5132187
Shaquille <shaqdulove@hotmail.com>
2025-06-07 18:48:44 -0400

    useDeviceOrientation outdated dimensions bug fix

--- src/essentials/hooks/useDeviceOrientation.ts
+++ src/essentials/hooks/useDeviceOrientation.ts
@@ -1,34 +1,23 @@
 import { useState, useEffect } from 'react';
 import { Dimensions } from 'react-native';
 import {
-  initialOrientation,
-  SCREEN_HEIGHT,
-  SCREEN_WIDTH,
   type DeviceOrientation as Orientation,
   type ScaledSizeDimensions,
 } from '../constants/device.const';
-import { relativeLong, relativeShort, relativeX, relativeY } from '../utils';
 
 export type DeviceOrientation = {
-  relativeY: (n: number) => number;
-  relativeX: (n: number) => number;
-  relativeLong: (n: number) => number;
-  relativeShort: (n: number) => number;
   screenWidth: number;
   screenHeight: number;
   orientation: Orientation;
 };
 export const useDeviceOrientation = () => {
+  const SCREEN_WIDTH = Dimensions.get('screen').width;
+  const SCREEN_HEIGHT = Dimensions.get('screen').height;
   const [orientation, setOrientation] = useState<DeviceOrientation>({
-    orientation: initialOrientation,
+    orientation: SCREEN_WIDTH > SCREEN_HEIGHT ? 'landscape' : 'portrait',
     screenHeight: SCREEN_HEIGHT,
     screenWidth: SCREEN_WIDTH,
-    relativeLong: relativeLong,
-    relativeShort: relativeShort,
-    relativeX: relativeX,
-    relativeY: relativeY,
   });
-
   useEffect(() => {
     const updateDimensions = (dimensions: ScaledSizeDimensions) => {
       //prettier-ignore
@@ -37,10 +26,6 @@ export const useDeviceOrientation = () => {
         orientation: width > height ? 'landscape' : 'portrait',
         screenHeight: height,
         screenWidth: width,
-        relativeLong: (num: number) => Math.max(width, height) * (num / 100),
-        relativeShort: (num: number) => Math.min(width, height) * (num / 100),
-        relativeX: (num: number) => width * (num / 100),
-        relativeY: (num: number) => height * (num / 100),
       });
     };
     const subscription = Dimensions.addEventListener(
