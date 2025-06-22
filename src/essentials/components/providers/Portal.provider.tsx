3ee9e380d5960bd47b0c53453f338054fc8b747a
Shaquille <shaqdulove@hotmail.com>
2025-06-22 16:24:54 -0400

    portal bug fix

--- src/essentials/components/providers/Portal.provider.tsx
+++ src/essentials/components/providers/Portal.provider.tsx
@@ -25,14 +25,25 @@ export const PortalContext = createContext<PortalContextValue | undefined>(
   undefined
 );
 
-export const PortalProvider = ({ children }: { children: ReactNode }) => {
+export type PortalItemStatus = 'mount' | 'update';
+export type PortalItemStatusTime = Partial<Record<PortalItemStatus, number>>;
+
+export const PortalProvider = ({
+  children,
+  unMountWaitBuffer,
+}: {
+  children: ReactNode;
+  unMountWaitBuffer?: number;
+}) => {
   const [portals, setPortals] = useState<PortalItem[]>([]);
-  const keys = useRef<Record<string, true>>({});
+  const keys = useRef<Record<string, PortalItemStatusTime>>({});
+
+  const unMountBuffer = unMountWaitBuffer || 500;
 
   const mount: PortalContextValue['mount'] = (key, element, onMount) => {
     if (keys.current[key]) return key;
     setPortals((prev) => [...prev, { key, element }]);
-    keys.current[key] = true;
+    keys.current[key] = { mount: Date.now() };
     onMount?.(key);
     return key;
   };
@@ -42,13 +53,18 @@ export const PortalProvider = ({ children }: { children: ReactNode }) => {
     setPortals((prev) =>
       prev.map((item) => (item.key === key ? { ...item, element } : item))
     );
+    keys.current[key].update = Date.now();
   };
 
   const unmount: PortalContextValue['unmount'] = (key, onUnMount) => {
     if (!keys.current[key]) return;
-    setPortals((prev) => prev.filter((item) => item.key !== key));
-    delete keys.current[key];
-    onUnMount?.(key);
+    setTimeout(() => {
+      const updateTime = keys.current[key]?.update || 0;
+      if (Date.now() - updateTime <= unMountBuffer) return;
+      setPortals((prev) => prev.filter((item) => item.key !== key));
+      delete keys.current[key];
+      onUnMount?.(key);
+    }, unMountBuffer);
   };
 
   return (
