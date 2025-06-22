5029561b01522e8d45a648f7feb5d488382be15b
Shaquille <shaqdulove@hotmail.com>
2025-06-21 20:52:53 -0400

    RowLayout bug fix

--- src/essentials/components/layouts/Row.layout.tsx
+++ src/essentials/components/layouts/Row.layout.tsx
@@ -1,12 +1,12 @@
 import type { PropsWithChildren } from 'react';
-import { View, type ViewStyle } from 'react-native';
-import type { LayoutProps } from './Layout';
+import { type ViewStyle } from 'react-native';
+import { Layout, type LayoutProps } from './Layout';
 
 export function RowLayout(props: PropsWithChildren<LayoutProps>) {
   const style: ViewStyle = { flexDirection: 'row' };
   return (
-    <View {...props} style={[props.style, style]}>
+    <Layout {...props} style={[props.style, style]}>
       {props.children}
-    </View>
+    </Layout>
   );
 }
