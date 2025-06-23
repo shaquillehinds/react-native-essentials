632d21030c5b6d2b9ce169f37c306fcb41e1705a
Shaquille <shaqdulove@hotmail.com>
2025-06-22 23:30:48 -0400

    portal bug fix

--- src/essentials/components/providers/Portal.provider.tsx
+++ src/essentials/components/providers/Portal.provider.tsx
@@ -25,20 +25,34 @@ export const PortalContext = createContext<PortalContextValue | undefined>(
   undefined
 );
 
-export type PortalItemStatus = 'mount' | 'update';
+export type PortalItemStatus = 'mount' | 'update' | 'unmount';
 export type PortalItemStatusTime = Partial<Record<PortalItemStatus, number>>;
 
 export const PortalProvider = ({
   children,
-  unMountWaitBuffer,
+  unMountBufferTimeMS,
+  updateBufferTimeMS,
 }: {
   children: ReactNode;
-  unMountWaitBuffer?: number;
+  /**
+   * Determines how much time in millieseconds to wait before removing portal item.
+   * Useful for stopping premature portal item ejection due to content changes
+   * @default 500 milliseconds
+   */
+  unMountBufferTimeMS?: number;
+  /**
+   * How much time to wait until portal update can be called again.
+   * Useful for stopping infinite portal update feedback loops.
+   * updateBufferTimeMS * portals.length
+   * @default 15 milliseconds
+   */
+  updateBufferTimeMS?: number;
 }) => {
   const [portals, setPortals] = useState<PortalItem[]>([]);
   const keys = useRef<Record<string, PortalItemStatusTime>>({});
 
-  const unMountBuffer = unMountWaitBuffer || 500;
+  const unMountBuffer = unMountBufferTimeMS || 100;
+  const updateBuffer = (updateBufferTimeMS || 15) * portals.length;
 
   const mount: PortalContextValue['mount'] = (key, element, onMount) => {
     if (keys.current[key]) return key;
@@ -50,6 +64,11 @@ export const PortalProvider = ({
 
   const update = (key: PortalKey, element: ReactNode) => {
     if (!keys.current[key]) return;
+    if (
+      keys.current[key].update &&
+      Date.now() - keys.current[key].update <= updateBuffer
+    )
+      return;
     setPortals((prev) =>
       prev.map((item) => (item.key === key ? { ...item, element } : item))
     );
@@ -58,9 +77,11 @@ export const PortalProvider = ({
 
   const unmount: PortalContextValue['unmount'] = (key, onUnMount) => {
     if (!keys.current[key]) return;
+    keys.current[key].unmount = Date.now();
     setTimeout(() => {
       const updateTime = keys.current[key]?.update || 0;
-      if (Date.now() - updateTime <= unMountBuffer) return;
+      const unmountTime = keys.current[key]?.unmount || 0;
+      if (updateTime > unmountTime) return;
       setPortals((prev) => prev.filter((item) => item.key !== key));
       delete keys.current[key];
       onUnMount?.(key);
